29aa9515e44475a92c3b0b641c4eb0c12fb80c5f
davidliang781205 <davidliang781205@gmail.com>
2017-04-18 13:25:43 -0700

    Completed product spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature do
+# SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Product details", js: true do
+    # ACT
+    visit root_path
+    # DEBUG
+    products = page.all('footer')
+    within(products[0]) do
+      expect(page).to have_link('Details')
+    end
+    save_screenshot
+
+    # VERIFY
+    # expect(page).to have_css 'article.product'
+  end
+end
